a5137343d1a3084db3cfd39e3c04ebe6107aff54
PISIT\pisit <hnjdragontung@gmail.com>
2026-03-02 17:55:51 +0700

    trypredicr2

---
+++ src/app/api/predict2/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server"
+
+export async function POST(req: Request) {
+  try {
+   
+    const body = await req.json()
+    const controller = new AbortController()
+    const timeoutId = setTimeout(() => controller.abort(), 60000)
+
+    const response = await fetch(
+      `http://127.0.0.1:8000/signal?symbol=${body.symbol}&timeframe=${body.timeframe}&nameEA=${body.nameEA}`,
+      {
+        method: "GET",
+        signal: controller.signal
+      }
+    )
+
+   clearTimeout(timeoutId)
+   
+    if (!response.ok) {
+      return NextResponse.json({
+        signal: "HOLD",
+        error: "AI_SERVER_ERROR",
+      })
+    }
+
+    // รับ response จาก AI
+    const aiResult = await response.json()
+
+    // ส่งกลับให้ EA
+    console.log(aiResult)
+    return NextResponse.json(aiResult)
+
+  } catch (err) {
+    console.error("Predict Error:", err)
+
+    return NextResponse.json({
+      signal: "HOLD",
+      error: "SERVER_ERROR",
+    })
+  }
+}
